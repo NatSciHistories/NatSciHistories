83686b7d10197ec9791156fbf45809e5a22bfe16
NatSciLaboratories <ramon.figueroa15@upr.edu>
2025-02-16 03:25:04 -0400

    Design changes and pagefind setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
+    integrations: [pagefind()],
     output: "static", // Ensures static build
     site: 'https://natscilaboratories.github.io/NatSciLaboratories',
     base: '/NatSciLaboratories/',
